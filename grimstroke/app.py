ec32d2852d1a87126c9cd5760bd215867588d561
masoodkamyab <masoodkamyab@users.noreply.github.com>
2022-12-18 16:05:58 +0000

    ✅ Ready to clone and code.

--- grimstroke/app.py
+++ grimstroke/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+grimstroke API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="grimstroke",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://grimstroke.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "masoodkamyab",
+        "url": "http://grimstroke.com/contact/",
+        "email": "masoodkamyab@grimstroke.com",
     },
     license_info={
         "name": "The Unlicense",
